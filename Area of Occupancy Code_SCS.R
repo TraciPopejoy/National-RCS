b426bc81c9cddf31cfc0395e00ff7af06a9c4f88
Traci P. DuBose <TraciPopejoy@ou.edu>
2020-07-01 17:25:19 -0400

    loads all occurrence data into data.frames and joins them into a monster df

--- Area of Occupancy Code_SCS.R
+++ Area of Occupancy Code_SCS.R
@@ -1,30 +1,34 @@
 # Area of Occupancy Code for the National Fishes Vulnerability Assessment - "Area of Occupancy Code_SCS.R"
 # Revised by Sam Silknetter, 03April2020
+# edited on 2020July01 by TPD
+
+### NOTES: need to download HUC12 shapefile - look for original source to match Sam&Jen&Abby
+###       currently have all HUC12 lines commented out
+###       if we want to look at rcs through time, might want to:
+                # 1. pull in data
+                # 2. apply time breaks
+                # 3. build AOOs & CS score based on the smaller sample (filter function)
+                # 4. AOO & CS table will both species & period identifier
 
 # Install necessary libraries.
-library(raster)
-library(rgbif)
-library(rgdal)
-library(sp)
-library(rgeos)
-library(scales)
+library(raster);library(rgdal);library(sp);library(rgeos);library(scales)
 
 # Set data paths.
-PATH_HUC12 <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/HUC12"
+#PATH_HUC12 <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/HUC12"
   # Use only the subset of candidate spp. that met filtering requirements or are 'exception species'. These are the 'focal species'.  
-PATH_FocalSpecies_OccurrenceData <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Focal Species_Occurrence Data/"
-PATH_SHP_HUC12 <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Shapefiles/HUC-12/"
-PATH_SHP_1km <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Shapefiles/1km Buffer/"
-PATH_SHP_dis1km <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Shapefiles/Dissolved 1km Buffer/"
+PATH_FocalSpecies_OccurrenceData <- "occ_data"
+#PATH_SHP_HUC12 <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Shapefiles/HUC-12/"
+PATH_SHP_1km <- "rcs_results/1kmbuf/"
+PATH_SHP_dis1km <- "rcs_results/dis1km/"
 
 # Set spatial coordinate reference system (CRS).
 crs.geo <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
 crs.albers <- CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=km +no_defs")
 
 # Read in fixed data.
-huc12 <- readOGR(dsn = PATH_HUC12, layer = "HUC12")
-proj4string(huc12) <- CRS("+init=epsg:4269 +proj=longlat +ellps=GRS80 +datum=NAD83")
-huc12_wgs84 <- spTransform(huc12, crs.geo)
+#huc12 <- readOGR(dsn = PATH_HUC12, layer = "HUC12")
+#proj4string(huc12) <- CRS("+init=epsg:4269 +proj=longlat +ellps=GRS80 +datum=NAD83")
+#huc12_wgs84 <- spTransform(huc12, crs.geo)
 
 # Create empty AOO data table to be populated.
 AOOs <- data.frame(scientific_name = character(), buffer_1km = numeric(), dissolved_buffer_1km = numeric(), 
@@ -32,27 +36,38 @@ AOOs <- data.frame(scientific_name = character(), buffer_1km = numeric(), dissol
         cv = numeric(), logBuffer1km = numeric(), logHUC12 = numeric(), stringsAsFactors = F)
 
 # Create a list of occurrence data files for all focal species. 
-FILES <- list.files(path=PATH_FocalSpecies_OccurrenceData)
+#FILES <- list.files(path=PATH_FocalSpecies_OccurrenceData)
+source('LoadingOccDataframes.R') #load the occurrence datasets
+anuran_taxa<-unique(Anurans_df$species)
 
-# Loop to cycle through all focal species (N = 144).
-  # for(i in 62:62){ ##Hint for testing with one species, set i=1
-for(i in 1:length(FILES)){
-  geodata <- read.csv(paste(PATH_FocalSpecies_OccurrenceData,"/", FILES[i], sep = ""), header=TRUE)
+# Loop to cycle through all species
+orig.time<-paste(Sys.time())
+#for(i in 1:length(unique(anuran_taxa))){ 
+  
+for(i in 1:5){
+  #problem: some species have multiple names within their datasheet - this causes problems!
+  
+  
+  #load the data into geodata [!not split by time yet]
+  geodata <- Anurans_df %>% 
+    filter(species==unique(anuran_taxa)[i]) %>% # only pull out one taxa (matches i)
+    dplyr::select(family, genus, species, Longitude, Latitude, year, source, #important columns
+           gbifID, ID, UUID)#keeping these columns just in case
 
   # Convert from 'geodata' CSV to an OGR object. 
   dat_csv <- as.data.frame(geodata)
-  dat_coord <- dat_csv[c("decimalLongitude", "decimalLatitude")] # Create data frame of longitude and latitude for all entries.
+  dat_coord <- dat_csv[c("Longitude", "Latitude")] # Create data frame of longitude and latitude for all entries.
   dat_sp <- SpatialPointsDataFrame(dat_coord, dat_csv, proj4string = crs.geo) # Save spatial dataframe with correct projection.
   
   # Set column 1 in AOOs to species name.
   AOOs[i,1] <- as.character(dat_sp$species[1])  
   
   # Calculate area (km^2) of occupied watersheds per species. 
-  dat_sp$HUC12 <- over(dat_sp, huc12_wgs84)$HUC12 # Store the HUC12 name as an attribute of the fish data.
-  Number_of_HUC12s <- data.frame(unique(dat_sp$HUC12))
-  huc12_sp <- huc12[huc12$HUC12 %in% Number_of_HUC12s$unique.dat_sp.HUC12.,]  # Extract unique HUC12 data for the species.
-  total_area_huc12 <- sum(huc12_sp$AreaSqKm) # Sum the total area (square kilometer) for each unique HUC12.
-  AOOs[i,]$huc12_area <- total_area_huc12
+  #dat_sp$HUC12 <- over(dat_sp, huc12_wgs84)$HUC12 # Store the HUC12 name as an attribute of the fish data.
+  #Number_of_HUC12s <- data.frame(unique(dat_sp$HUC12))
+  #huc12_sp <- huc12[huc12$HUC12 %in% Number_of_HUC12s$unique.dat_sp.HUC12.,]  # Extract unique HUC12 data for the species.
+  #total_area_huc12 <- sum(huc12_sp$AreaSqKm) # Sum the total area (square kilometer) for each unique HUC12.
+  #AOOs[i,]$huc12_area <- total_area_huc12
   
   # Generate 1km point buffers. You must use a projected CRS for function gBuffer(). For CRS, we use: USA Contiguous albers equal area. 
   geodata_Albers <- spTransform(dat_sp, crs.albers)
@@ -65,16 +80,17 @@ for(i in 1:length(FILES)){
   
   # Save the spatial dataframes as ESRI Shapefiles. 
   species <- geodata$species[1] # Identify the species name for CSV output. 
-  raster::shapefile(huc12_sp, file = paste0(PATH_SHP_HUC12, species, "_HUC12.shp"), overwrite=TRUE)
+  #raster::shapefile(huc12_sp, file = paste0(PATH_SHP_HUC12, species, "_HUC12.shp"), overwrite=TRUE)
   raster::shapefile(sp_buffer_1km, file = paste0(PATH_SHP_1km, species, "_1km.shp"), overwrite=TRUE)
   raster::shapefile(dissolved_1km_buffer, file = paste0(PATH_SHP_dis1km, species, "_1km.shp"), overwrite=TRUE)
 }
 
+
 # Calculate AOO rank for each range metric and focal species.
 AOOs$rank1km <- rank(AOOs$dissolved_buffer_1km)
-AOOs$rank_huc12 <- rank(AOOs$huc12_area)
+#AOOs$rank_huc12 <- rank(AOOs$huc12_area)
 
-for(i in 1:length(FILES)){
+for(i in 1:30){
   # Calculate and store mean of the 2 AOO ranks in the mean column.
   AOOs[i,]$mean_Rank <- mean(c(AOOs[i,]$rank1km, AOOs[i,]$rank_huc12), na.rm=TRUE)
 
@@ -92,8 +108,10 @@ for(i in 1:length(FILES)){
 
   # Calculate and store logarithms (base 10) of AOOs for both range metrics.
   AOOs$logBuffer1km <- log10(AOOs$dissolved_buffer_1km)
-  AOOs$logHUC12 <- log10(AOOs$huc12_area)
+  #AOOs$logHUC12 <- log10(AOOs$huc12_area)
+  print(AOOs[i,]$scientific_name)
 }
 
 # Write CSV with date in "_01Jan2020" format.
-write.csv(AOOs, file = "AOO Output_03April2020.csv")
+write.csv(AOOs, file = paste0("rcs_results/AOO Output_",
+                              format(Sys.Date(), '%Y%m%d'),'.csv'))
---
+++ LoadingOccDataframes.R
@@ -0,0 +1,49 @@
+# Script for loading occurance data quickly
+# last revised 2020July01 tpd
+
+#load libraries
+library(tidyverse); library(lubridate)
+library(sf); library(rgeos); library(sp); library(rgdal)
+library(cowplot)
+
+#list.files('G:/Shared drives/Anuran Biodiversity Project/SDMs/Occurrence Data/Download & Filtering/')[1:4]
+main_dir<-"occ_data" #where the occurrence data resides, one folder, one csv for each taxa
+taxa<-gsub('_.*','',list.files(main_dir)) #list the taxa we have occurrence data for
+
+
+for(q in taxa){
+  occ_file_name<-list.files(main_dir, pattern=q, full.names = T)
+  
+  #assigning each csv to the correct G.species object
+  assign(paste(substr(q,1,1), #pull genus inital out
+               gsub("[A-z ]*\\.","", q), #keep everything after the first space
+               sep='.'),
+         read.csv(occ_file_name) %>% #read the file in
+           dplyr::select(-kingdom,-phylum,-class, #don't need
+                         -recordNumber, -individualCount, -catalogNumber) %>% #drop for ease of binding the rows together
+           mutate(tax=paste(substr(q,1,1), gsub("[A-z ]*\\.","", q),sep="."))) #add a column to indicate main taxa name
+  #check this actually worked correctly
+  print(paste(paste(substr(q,1,1), gsub("[A-z ]*\\.","", q), sep='.'), #object name
+              unique(get(paste(substr(q,1,1), gsub("[A-z ]*\\.","", q),#species within that object name
+                               sep='.'))$species), sep="=="))
+
+}
+
+Anurans_df<-bind_rows(mget(paste(substr(taxa,1,1), #pull genus inital out
+                                 gsub("[A-z ]*\\.","", taxa), #keep everything after the period
+                                 sep='.')))
+
+
+nobs_source_table<-Anurans_df %>% group_by(tax, species, source) %>% tally() %>%
+  pivot_wider(names_from = source, values_from = n)
+write.csv(nobs_source_table, paste0('rcs_results/nobs_source_table_',
+                                   format(Sys.Date(), '%Y%m%d'),'.csv'))
+
+# need to ask Chloe &/or Meryl about the following species
+double_sp<-Anurans_df %>% group_by(tax) %>% 
+  summarize(all.spp=paste(unique(species), collapse=", ")) %>%
+  group_by(tax, all.spp) %>%
+  dplyr::select(tax, all.spp) %>%
+  filter(grepl(', ', all.spp))
+write.csv(double_sp, paste0('rcs_results/double_sp_',
+                                    format(Sys.Date(), '%Y%m%d'),'.csv'))
